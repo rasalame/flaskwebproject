3cc6c2502c1afaab50e211e35010b84441f5a37f
unknown <ramisalame@hotmail.com>
2022-03-12 10:40:24 -0500

    First commit

---
+++ flaskproject.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+
+
+
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'rami',
+        'title': 'ramis post',
+        'content': 'first post by rami'
+        },
+        {
+        'author': 'mary',
+        'title': 'marys post',
+        'content': 'first post by mary'
+        }
+    ]
+
+
+
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
